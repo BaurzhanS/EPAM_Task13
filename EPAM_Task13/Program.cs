3969ab5b0f6b6c581708afd9811dc4bf618c7dfa
baurik82 <baurik82@mail.ru>
2019-10-28 20:29:47 +0600

    Add project files.

---
+++ EPAM_Task13/Program.cs
@@ -0,0 +1,66 @@
+﻿using EPAM_Task13.Task1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataProcess process = new DataProcess();
+            Tree upload = new Tree();
+            
+            Node root = null;
+            root = upload.Insert(root, 1, "Саша", "Математика", "1 января 2019 г.", "B+");
+            root = upload.Insert(root, 2, "Стас", "География", "5 января 2019 г.", "C+");
+            root = upload.Insert(root, 3, "Екатерина", "Биология", "25 февраля 2019 г.", "A+");
+            root = upload.Insert(root, 4, "Вероника", "Высшая Математика", "24 марта 2019 г.", "A+");
+            root = upload.Insert(root, 5, "Туран", "Геометрия", "26 апреля 2019 г.", "A+");
+            root = upload.Insert(root, 6, "Дженнифер", "Логика", "2 марта 2019 г.", "D");
+            root = upload.Insert(root, 7, "Безос", "Программирование", "24 марта 2019 г.", "D");
+
+            //process.WriteStudentToFile(upload.nodes);
+
+            Tree students = process.ReadStudentsFromFile();
+
+            Console.WriteLine("Выберите соответствующую цифровую команду:");
+            int command;
+            Console.WriteLine(@"1-Найти студента по имени; 2-Найти студентов по оценкам; 3-Отсортировать студентов по имени");
+            command = Convert.ToInt32(Console.ReadLine());
+            switch (command)
+            {
+                case 1:
+                    Console.WriteLine("Введите имя студента ");
+                    string name = Console.ReadLine();
+                    var studentByName = students.nodes.FirstOrDefault(f => f.name == name);
+                    Console.WriteLine(studentByName.name);
+                    Console.WriteLine(studentByName.testName);
+                    Console.WriteLine(studentByName.testMark);
+                    Console.WriteLine(studentByName.testDate);
+                    break;
+                case 2:
+                    Console.WriteLine("Введите оценку ");
+                    string mark = Console.ReadLine();
+                    var studentsByMark = students.nodes.Where(w=>w.testMark==mark).ToList();
+                    foreach (var student in studentsByMark)
+                    {
+                        Console.WriteLine(student.name);
+                    }
+                    break;
+                case 3:
+                    var studentsSorted = students.nodes.OrderBy(o => o.name);
+                    foreach (var student in studentsSorted)
+                    {
+                        Console.WriteLine($"{student.name} {student.testMark} ");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ EPAM_Task13/Task1/Student.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_Task13.Task1
+{
+    class DataProcess
+    {
+        string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"students.txt");
+
+        public void WriteStudentToFile(List<Node> nodes)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                foreach (Node s in nodes)
+                {
+                    writer.Write(s.id);
+                    writer.Write(s.name);
+                    writer.Write(s.testName);
+                    writer.Write(s.testDate);
+                    writer.Write(s.testMark);
+                }
+            }
+        }
+
+        public Tree ReadStudentsFromFile()
+        {
+            Tree treeStudents = new Tree();
+
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                while (reader.PeekChar() > -1)
+                {
+                    Node student = new Node();
+
+                    student.id = reader.ReadInt32();
+                    student.name = reader.ReadString();
+                    student.testName = reader.ReadString();
+                    student.testDate = reader.ReadString();
+                    student.testMark = reader.ReadString();
+
+                    treeStudents.nodes.Add(student);
+                }
+            }
+            return treeStudents;
+        }
+    }
+
+    class Node
+    {
+        public int id;
+        public string name;
+        public string testName;
+        public string testDate;
+        public string testMark;
+
+        public Node left;
+        public Node right;
+    }
+
+    class Tree
+    {
+        public List<Node> nodes = new List<Node>();
+
+        public Node Insert(Node root, int id, string name, string testName, string testDate, string testMark)
+        {
+            if (root != null && !nodes.Contains(root))
+            {
+                nodes.Add(root);
+            }
+
+            if (root == null)
+            {
+                root = new Node();
+                root.id = id;
+                root.name = name;
+                root.testName = testName;
+                root.testDate = testDate;
+                root.testMark = testMark;
+            }
+            else if (id < root.id)
+            {
+                root.left = Insert(root.left, id, name, testName, testDate, testMark);
+
+                if (root != null && !nodes.Contains(root.left))
+                {
+                    nodes.Add(root.left);
+                }
+            }
+            else
+            {
+                root.right = Insert(root.right, id, name, testName, testDate, testMark);
+
+                if (root != null && !nodes.Contains(root.right))
+                {
+                    nodes.Add(root.right);
+                }
+            }
+
+            return root;
+        }
+
+        public Node Find(Node root, int id)
+        {
+            if (root != null)
+            {
+                if (id == root.id) return root;
+
+                if (id < root.id)
+                    return Find(root.left, id);
+                else
+                    return Find(root.right, id);
+            }
+            return null;
+        }
+    }
+
+
+
+}
